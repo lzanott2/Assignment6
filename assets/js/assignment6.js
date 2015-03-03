1e848c66f2b82e87b4a367db1706987e231d9a11
KittyGirl <lzanott2@du.edu>
2015-03-03 04:02:36 -0700

    Update assignment6.js

--- assets/js/assignment6.js
+++ assets/js/assignment6.js
@@ -1,51 +1,45 @@
+var searchUrl = 'http://api.bing.net/qson.aspx?Query=';
+var endUrl = '&JsonType=callback&JsonCallback=?';
 
-var bingKey = 'AmNIY8wGKFfCuCl/NVwA7uG6JfW+gowaYz/Ip+noCWA'; //bing key, just for me, Lor Z
-var searchUrl = 'http://api.bing.net/qson.aspx?Query='; //bing search link
 
+$('input').on('keyup',function(evt) {
+	var userInput = $('input').val();
+	
+	$.ajax({
+		url : searchUrl + userInput + endUrl,
+		type: 'GET',
+		dataType: 'jsonp'
+	})
 
-$('input').on('keyup', function (evt) {
-		getSearches($(this).val());
+	.done (function(response) {
+		$('#results').empty();
+		var results = response.SearchSuggestion.Section;
+		
+		$.each(results, function(i, value)	{
+			var html = $('<a>' + value.Text + '</a>');
+			
+			$('a').attr('href', encodeURI('http://www.bing.com/search?q=' + value.Text));
+			
+			console.log(value.Text);
+
+			$('#results').append(html);
+		});
+	});
 });
 
 
-function getSearches(query) {
-	var q = query;
-	//var pageLimit = 10;
-	var url = encodeURI(searchUrl + query + '&JsonType=callback&JsonCallback=?');
-		    
-
-	console.log(url);
-
-	$.ajax({
-		url: url,
-		dataType: 'jsonp',
-	})
-
-	.done(function(response) {
-		console.log(response);
-		render(response.SearchSuggestion.Section); //this is calling the render function and like saying that searches equates all this in the parameter for render.
-	});
-}
-
-
-function render(searches) {
-	var results = $('#results');
-	$(results).empty();
-	var section = $('section');
-	//var ul = $('ul');    Why doesn't this work?
-
-	for(var i = 0; i < searches.length; i++) {
-		//saves all obejects in the search array to the results id
-		results.append(searches[i]); 
+	// for(var i = 0; i < searches.length; i++) {
+	// 	//saves all obejects in the search array to the results id
+	// 	results.append(searches[i]); 
 		
-		var text1 = $("<div></div>").text(searches[i].Text);
-		//var text1 = $("<li></li>").text(searches[i].Text);    Why doesn't this work?
+	// 	var text1 = $("<div></div>").text(searches[i].Text);
+	// 	//var text1 = $("<li></li>").text(searches[i].Text);    Why doesn't this work?
 
-		$(section).append(text1);
-		//$(ul).append(text1);    Why doesn't this work?
+	// 	$(section).append(text1);
+	// 	//$(ul).append(text1);    Why doesn't this work?
 	
-		console.log(searches[i].Text);
-	}
+	// 	console.log(searches[i].Text);
+	// }
 
 
 	// Not sure how to use this!!!!!!
@@ -67,6 +61,3 @@ function render(searches) {
 	//    		"resizable,scrollbars,status"
 	//  	);
 	// }
-}
-
-
